bdc5381120d60abaf167c316753896c2e9e22892
Roman Mashenkin <xromash@vxdesign.store>
2019-05-19 23:51:33 +0300

    Implement a possibility to show merged cells and borders
    Signed-off-by: Roman Mashenkin <xromash@vxdesign.store>

--- Example_07_01.m
+++ Example_07_01.m
@@ -2,6 +2,7 @@
 function Example_07_01
 [hSheet, hWorkBook] = xls_open('Лекция 7. Работа с Excel');
 NameRows = {'Вариант 1'; 'Вариант 2'};
-NameCols = {'№', 'f(x)', 'x1', 'x2', 'x3'};
-TableData = {1, 2, 'нет', 4; 'да', 6, 'ошибка', 8};
-xls_insert_table(hSheet, [1 1], NameCols, NameRows, TableData);
+NameCols = {'N' 'f(x)', 'X', '', ''; '', '', 'x1', 'x2', 'x3'};
+TableData = {1, 2, 'нет', 4; 'да', 6, 'отказ', 8};
+Merge = [1 1 2 1; 1 2 2 1; 1 3 1 3];
+xls_insert_table(hSheet, [1 1], NameCols, NameRows, TableData, Merge);
--- excel_functions/xls_insert_table.m
+++ excel_functions/xls_insert_table.m
@@ -1,26 +1,60 @@
-function xls_insert_table(hSheet, RowCol, NameCols, NameRows, TableData)
-NameRowsSize = ~isempty(NameRows);
-NameColsSize = ~isempty(NameCols);
+% Merge - [Row Col RowCount ColCount]
+function xls_insert_table(hSheet, RowCol, NameCols, NameRows, TableData, varargin)
+Merge = [];
+
+if ~isempty(varargin), Merge = varargin{1}; end
+NameRowsSize = size(NameRows,2);
+NameColsSize = size(NameCols,1);
+h = h_xls;
 
 if ~isempty(NameCols)
     hRange = xls_insert(hSheet, ...
-        RowCol + [0, numel(NameCols) <= size(TableData, 1)], NameCols);
+        RowCol + [0, size(NameCols, 2) <= size(TableData, 2)], NameCols);
     hRange.Font.FontStyle = 'полужирный';
+    hRange.Select;
+    h.Selection.HorizontalAlignment = -4108;
+    h.Selection.VerticalAlignment = -4108;
 end
 
 if ~isempty(NameRows)
     hRange = xls_insert(hSheet, RowCol + [NameColsSize, 0], NameRows);
     hRange.Font.FontStyle = 'полужирный';
+    hRange.Select;
+    h.Selection.HorizontalAlignment = -4108;
+    h.Selection.VerticalAlignment = -4108;
 end
 
 if ~isempty(TableData)
-    hRange = xls_insert(hSheet, RowCol + [NameColsSize, NameRowsSize], TableData);
-    hRange.Font.FontStyle = 'обычный';
+   hRange = xls_insert(hSheet, RowCol + [NameColsSize, NameRowsSize], ...
+       TableData);
+   hRange.Font.FontStyle = 'обычный';
+   hRange.Select;
+   h.Selection.HorizontalAlignment = -4152;
+   h.Selection.VerticalAlignment = -4108;
 end
 
-RowColEnd = RowCol + [NameRowsSize NameColsSize] + size(TableData) - 1;
+for i=1:size(Merge, 1)
+    [hRange] = xls_Range_by_StartEnd(hSheet, Merge(i, 1:2), ...
+        Merge(i, 1:2) + (Merge(i, 3:4) - 1));
+    hRange.Select;
+    h.Selection.Merge;
+end
+
+RowColEnd = RowCol + [NameColsSize NameRowsSize] + size(TableData) - 1;
 [hRange] = xls_Range_by_StartEnd(hSheet, RowCol, RowColEnd);
 hRange.Select;
-h = h_xls;
 h.Selection.Rows.AutoFit;
 h.Selection.Columns.AutoFit;
+ColWidth = zeros(1, RowColEnd(2));
+
+for i = (RowCol(2) + NameRowsSize):RowColEnd(2)
+    ColWidth(i) = h.Column.Item(i).ColumnWidth;
+end
+
+for i = (RowCol(2) + NameRowsSize):RowColEnd(2)
+    h.Column.Item(i).ColumnWidth = max(ColWidth);
+end
+
+for i = 1:4
+    h.Selection.Borders.Item(i).LineStyle=1;
+end
